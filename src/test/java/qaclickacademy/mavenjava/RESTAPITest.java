295e2473558bf425351b4deb3ca7fb1f9633490f
ravi80891 <ravindrajoda27@gmail.com>
2022-02-09 12:23:54 +0530

    First message

---
+++ src/test/java/qaclickacademy/mavenjava/RESTAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void PostJira() {
+		System.out.println("PostJira");
+		
+	}
+	@Test
+	public void DeleteTwitter() {
+		System.out.println("DeleteTwitter");
+		
+	}
+
+}
---
+++ src/test/java/qaclickacademy/mavenjava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+		
+	}
+	@Test
+	public void ElementsUi() {
+		System.out.println("ElementsUi");
+		
+	}
+
+}
