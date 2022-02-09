24b39fec34aa5965452355936986df6fb336e250
ravi80891 <ravindrajoda27@gmail.com>
2022-02-09 13:10:55 +0530

    this is commit from asian time guy

--- src/test/java/qaclickacademy/mavenjava/RESTAPITest.java
+++ src/test/java/qaclickacademy/mavenjava/RESTAPITest.java
@@ -6,12 +6,15 @@ public class RESTAPITest {
 	@Test
 	public void PostJira() {
 		System.out.println("PostJira");
-		
+		System.out.println("PostJira2");
+		System.out.println("PostJira3");
+
 	}
+
 	@Test
 	public void DeleteTwitter() {
 		System.out.println("DeleteTwitter");
-		
+
 	}
 
 }
